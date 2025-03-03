8aac00ad997d4391217421cd8fba7716a4ac934c
hayapo <39045817+hayapo@users.noreply.github.com>
2025-03-03 23:47:17 +0900

    feat: Add CRUD feat with drizzle

--- src/server/feeds.ts
+++ src/server/feeds.ts
@@ -1,4 +1,9 @@
+import { zValidator } from "@hono/zod-validator";
+import { eq } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
+import { feeds } from "src/db/schema";
+import { z } from "zod";
 
 export interface Feed {
 	id: number;
@@ -7,30 +12,68 @@ export interface Feed {
 }
 
 const app = new Hono<{ Bindings: { DB: D1Database } }>()
+	// すべてのフィードを取得
 	.get("/", async (c) => {
-		return c.json<Feed[]>(
-			[
-				{ id: 1, title: "Hello, World!", url: "/api/feeds/1" },
-				{ id: 2, title: "Hello, World!", url: "/api/feeds/2" },
-			],
-			200,
-		);
+		const db = drizzle(c.env.DB);
+		const found = await db.select().from(feeds).all();
+		console.log(found);
+		if (found.length === 0) {
+			return c.json({ error: "Not Found" }, 404);
+		}
+		return c.json<Feed[]>(await db.select().from(feeds).all(), 200);
 	})
-	.get("/:id", async (c) => {
-		return c.json<Feed>(
-			{ id: 1, title: "Hello, World!", url: "/api/feeds/1" },
-			200,
-		);
-	})
-	.post("/", async (c) => {
-		return c.json<Feed>(
-			{ id: 1, title: "Hello, World!", url: "/api/feeds/1" },
-			200,
-		);
-	})
-	.delete("/:id", async (c) => {
-		return c.text("OK");
-	})
+	// idを指定して特定のフィードを取得
+	.get(
+		"/:id",
+		zValidator(
+			"param",
+			z.object({
+				id: z.coerce.number(),
+			}),
+		),
+		async (c) => {
+			const { id } = c.req.valid("param");
+			const db = drizzle(c.env.DB);
+			const found = await db.select().from(feeds).where(eq(feeds.id, id));
+			if (found.length === 0) {
+				return c.json({ error: "Not Found" }, 404);
+			}
+			return c.json<Feed>({ ...found[0] }, 200);
+		},
+	)
+	// フィードを登録する
+	.post(
+		"/",
+		zValidator(
+			"form",
+			z.object({
+				title: z.string(),
+				url: z.string(),
+			}),
+		),
+		async (c) => {
+			const db = drizzle(c.env.DB);
+			const params = c.req.valid("form");
+			const res = await db.insert(feeds).values(params).returning();
+			return c.json<Feed>(res[0], 200);
+		},
+	)
+	// idを指定して特定のフィードを削除する
+	.delete(
+		"/:id",
+		zValidator(
+			"param",
+			z.object({
+				id: z.coerce.number(),
+			}),
+		),
+		async (c) => {
+			const db = drizzle(c.env.DB);
+			const { id } = c.req.valid("param");
+			await db.delete(feeds).where(eq(feeds.id, id));
+			return c.text("OK");
+		},
+	)
 	.onError((err, c) => {
 		console.error(err);
 		return c.text("Internal Server Error", 500);
