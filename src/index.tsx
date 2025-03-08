5003197007bd47fa57f41bde773611af3b256828
tbistr <tbistr99@gmail.com>
2025-03-08 15:33:49 +0900

    feat: Add proxy endpoint for CORS
    To fetch the feeds from another origin, we must use proxy to avoid CORS issue.

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,47 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 import { renderToString } from "react-dom/server";
 import api from "src/server";
 
 const app = new Hono();
 
 app.route("/api", api);
+app.use(logger());
 
-app.get("*", (c) => {
-	return c.html(
-		renderToString(
-			<html lang="en">
-				<head>
-					<meta charSet="utf-8" />
-					<meta content="width=device-width, initial-scale=1" name="viewport" />
-					<link
-						rel="stylesheet"
-						href="https://cdn.simplecss.org/simple.min.css"
-					/>
-					{import.meta.env.PROD ? (
-						<script type="module" src="/static/client.js" />
-					) : (
-						<script type="module" src="/src/client/client.tsx" />
-					)}
-				</head>
-				<body>
-					<div id="root" />
-				</body>
-			</html>,
-		),
-	);
-});
+app
+	.get("/cors-proxy", async (c) => {
+		const { url } = c.req.query();
+		console.log(url);
+		const response = await fetch(url);
+		const text = await response.text();
+		return c.text(text);
+	})
+	.get("*", (c) => {
+		return c.html(
+			renderToString(
+				<html lang="en">
+					<head>
+						<meta charSet="utf-8" />
+						<meta
+							content="width=device-width, initial-scale=1"
+							name="viewport"
+						/>
+						<link
+							rel="stylesheet"
+							href="https://cdn.simplecss.org/simple.min.css"
+						/>
+						{import.meta.env.PROD ? (
+							<script type="module" src="/static/client.js" />
+						) : (
+							<script type="module" src="/src/client/client.tsx" />
+						)}
+					</head>
+					<body>
+						<div id="root" />
+					</body>
+				</html>,
+			),
+		);
+	});
 
 export default app;
