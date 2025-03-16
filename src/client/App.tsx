1f1052cf2e64387256ac83a9c8ee52b9ebc56e4d
tbistr <tbistr99@gmail.com>
2025-03-16 15:32:12 +0900

    feat: Add article viewer

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,22 +1,59 @@
 import "@mantine/core/styles.css";
 
-import {} from "@mantine/core";
 import { MantineProvider } from "@mantine/core";
+import { AppShell, Burger } from "@mantine/core";
+import { Group, Skeleton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconBrandMantine } from "@tabler/icons-react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { ArticleViewer } from "./pages/ArticleViewer";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import { ShowRSSFeeds } from "./pages/ArticleViewer";
 import { FeedManager } from "./pages/FeedManager";
 import { NotFound } from "./pages/NotFound";
+
+function Layout() {
+	const [opened, { toggle }] = useDisclosure();
+
+	return (
+		<AppShell
+			header={{ height: 60 }}
+			navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+			padding="md"
+		>
+			<AppShell.Header>
+				<Group h="100%" px="md">
+					<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+					<IconBrandMantine size={30} />
+				</Group>
+			</AppShell.Header>
+
+			<AppShell.Navbar p="md">
+				Navbar
+				{Array(15)
+					.fill(0)
+					.map((_, index) => (
+						<Skeleton key={index} h={28} mt="sm" animate={false} />
+					))}
+			</AppShell.Navbar>
+
+			<AppShell.Main>
+				<Outlet />
+			</AppShell.Main>
+		</AppShell>
+	);
+}
 
 const root = createRoot(document.getElementById("root") ?? document.body);
 root.render(
 	<MantineProvider>
 		<BrowserRouter>
 			<Routes>
-				<Route index element={<NotFound />} />
-				<Route path="/articles" element={<ArticleViewer />} />
-				<Route path="/feeds" element={<FeedManager />} />
-				<Route path="/*" element={<NotFound />} />
+				<Route element={<Layout />}>
+					<Route index element={<NotFound />} />
+					<Route path="/articles" element={<ShowRSSFeeds />} />
+					<Route path="/feeds" element={<FeedManager />} />
+					<Route path="/*" element={<NotFound />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	</MantineProvider>,
--- src/client/pages/ArticleViewer.tsx
+++ src/client/pages/ArticleViewer.tsx
@@ -1,16 +1,54 @@
+import {
+	AspectRatio,
+	Button,
+	Card,
+	Container,
+	Image,
+	SimpleGrid,
+	Text,
+} from "@mantine/core";
+import type React from "react";
 import { useState } from "react";
 import Parser from "rss-parser";
+import classes from "./test.module.css";
 
-export const ArticleViewer = () => {
-	return <ShowRSSFeed />;
+type Item = {
+	title?: string;
+	link?: string;
+	thumbnail?: string;
+	pubDate?: string;
 };
 
-const ShowRSSFeed = () => {
-	type Item = {
-		title?: string;
-		link?: string;
-		pubDate?: string;
-	};
+const ArticleCards: React.FC<{ articles: Item[] }> = ({ articles }) => {
+	const cards = articles.map((article) => (
+		<Card
+			key={article.title}
+			p="md"
+			radius="md"
+			component="a"
+			href="#"
+			className={classes.card}
+		>
+			<AspectRatio ratio={1920 / 1080}>
+				<Image src={article.thumbnail} />
+			</AspectRatio>
+			<Text c="dimmed" size="xs" tt="uppercase" fw={700} mt="md">
+				{article.pubDate}
+			</Text>
+			<Text className={classes.title} mt={5}>
+				{article.title}
+			</Text>
+		</Card>
+	));
+
+	return (
+		<Container py="xl">
+			<SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>
+		</Container>
+	);
+};
+
+export const ShowRSSFeeds = () => {
 	const [rssItems, setRssItems] = useState<Item[]>([]);
 
 	const parser = new Parser();
@@ -23,39 +61,32 @@ const ShowRSSFeed = () => {
 			const res = await fetch(`/cors-proxy?${query}`);
 			const xml = await res.text();
 			const feed = await parser.parseString(xml);
-			setRssItems(feed.items);
+			const items = feed.items.map((item) => {
+				return {
+					title: item.title,
+					link: item.link,
+					thumbnail: item.enclosure?.url,
+					pubDate: item.pubDate,
+				};
+			});
+			setRssItems(items);
 		} catch (error) {
 			console.error("Failed to fetch RSS feed", error);
 		}
 	};
 
 	return (
-		<div>
-			<button type="button" onClick={handle}>
-				Do
-			</button>
-			<table border={1}>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Link</th>
-						<th>Published</th>
-					</tr>
-				</thead>
-				<tbody>
-					{rssItems.map((item, index) => (
-						<tr key={item.title + index.toString()}>
-							<td>{item.title}</td>
-							<td>
-								<a href={item.link} target="_blank" rel="noopener noreferrer">
-									Read More
-								</a>
-							</td>
-							<td>{item.pubDate}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+		<>
+			<div>
+				<Button onClick={handle}>Do</Button>
+			</div>
+			<div>
+				{rssItems.length > 0 ? (
+					<ArticleCards articles={rssItems} />
+				) : (
+					<Text size="xl">No articles</Text>
+				)}
+			</div>
+		</>
 	);
 };
