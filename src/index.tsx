12b3a4142774f9ba5862006cae495e008f6c6a58
tbistr <tbistr99@gmail.com>
2025-04-08 03:06:22 +0900

    fix: Use SPA mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,18 @@ import api from "@/server";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: { ASSETS: Fetcher } }>();
+app.use(logger());
 
 app.route("/api", api);
-app.use(logger());
+
+if (import.meta.env.PROD) {
+	// 本番ではASSETSへのリクエストをプロキシする
+	// 全てのリクエストにおいて、ASSETSから探してサーブする
+	// （なかったら勝手に404になる）
+	app.get("/*", (c) => {
+		return c.env.ASSETS.fetch(c.req.url);
+	});
+}
 
 export default app;
